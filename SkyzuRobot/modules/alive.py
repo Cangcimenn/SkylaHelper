db1c2bcc84e731a1efa8e534ae2b9165ee7cd8ec
Takichan X <92210483+Cangcimenn@users.noreply.github.com>
2022-01-10 11:27:41 +0700

    Update alive.py

--- SkyzuRobot/modules/alive.py
+++ SkyzuRobot/modules/alive.py
@@ -16,7 +16,7 @@ PHOTO = "https://telegra.ph/file/56811b69cbcece20bbebf.jpg"
 async def awake(event):
     TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Skyzu Robot.** \n\n"
     TEXT += "❂ **I'm Working Properly** \n\n"
-    TEXT += f"❂ **My Master : [Skyzu](https://t.me/skyzu)** \n\n"
+    TEXT += f"❂ **My Master : [Takichan](https://t.me/Zxyune)** \n\n"
     TEXT += f"❂ **Library Version :** `{telever}` \n\n"
     TEXT += f"❂ **Telethon Version :** `{tlhver}` \n\n"
     TEXT += f"❂ **Pyrogram Version :** `{pyrover}` \n\n"
@@ -24,7 +24,7 @@ async def awake(event):
     BUTTON = [
         [
             Button.url("ʜᴇʟᴘ​", "https://t.me/SkyzuRobot?start=help"),
-            Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/skyzusupport"),
+            Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/skylasupport"),
         ]
     ]
     await tbot.send_file(event.chat_id, PHOTO, caption=TEXT, buttons=BUTTON)
