a39b9c655ce1f6012ab465cc055a428ed0a94542
Takichan X <92210483+Cangcimenn@users.noreply.github.com>
2022-01-10 12:11:55 +0700

    Update inlinefuncs.py

--- SkyzuRobot/utils/inlinefuncs.py
+++ SkyzuRobot/utils/inlinefuncs.py
@@ -126,20 +126,20 @@ async def alive_function(answers):
     )
 
     msg = f"""
-**[Skyzu Robot ❤️](https://t.me/SkyzuRobot):**
+**[Skyla Helper ❤️](https://t.me/SkylaMusicBot):**
 **MainBot:** `{bot_state}`
 **UserBot:** `{ubot_state}`
 **Python:** `{pyver.split()[0]}`
 **Pyrogram:** `{pyrover}`
 **MongoDB:** `{mongover}`
 **Platform:** `{sys.platform}`
-**Profiles:** [BOT](t.me/{BOT_USERNAME}) | [UBOT](t.me/skyzu)
+**Profiles:** [BOT](t.me/{BOT_USERNAME}) | [UBOT](t.me/Zxyune)
 """
     answers.append(
         InlineQueryResultArticle(
             title="Alive",
             description="Check Bot's Stats",
-            thumb_url="https://telegra.ph/file/1b8f08dba704a5eb2bb09.jpg",
+            thumb_url="https://telegra.ph/file/5af32594e504236410892.jpg",
             input_message_content=InputTextMessageContent(
                 msg, disable_web_page_preview=True
             ),
